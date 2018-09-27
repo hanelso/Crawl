8ffc0127b43ae25b0405ae98b819a666b30879d1
hanelso <gjsxj1992@gmail.com>
2018-09-27 18:44:11 +0900

    파이썬 웹 크롤링2 -주석 추가!

--- Collect.py
+++ Collect.py
@@ -1,9 +1,5 @@
-from datetime import datetime
 from itertools import count
-from urllib.request import Request, urlopen
-
 from bs4 import BeautifulSoup
-
 from Collection.Crawler import crawling
 import pandas as pd
 
@@ -14,27 +10,28 @@ def crawling_pelicana():
     for page in count(start=1):
         html = crawling("http://www.pelicana.co.kr/store/stroe_search.html?branch_name=&gu=&si=&page=%d"%page)
 
-        bs = BeautifulSoup(html, "html.parser")
-        tag_table = bs.find('table', attrs={'class': 'table mt20'})
-        tag_tbody = tag_table.find('tbody')
-        tags_tr = tag_tbody.findAll('tr')
+        bs = BeautifulSoup(html, "html.parser")                     # BeautifulSoup으로 html 핸들링 라이브러리 이용.
+        tag_table = bs.find('table', attrs={'class': 'table mt20'}) # 받아온 html 문자열에서 table중에 class명이 'table mt20'인 것만 찾는다.
+        tag_tbody = tag_table.find('tbody')                         # table 안에서 tbody태그들만 찾는다.
+        tags_tr = tag_tbody.findAll('tr')                           # tbody 안에서 tr 태그들만을 모두 찾는다.
 
         # 페이지의 끝 확인
         if len(tags_tr) == 0:
             break
 
+        # tr 태그들을 하나씩 보면서 data들을 얻어낸다.
         for tag_tr in tags_tr:
             strings = list(tag_tr.strings)
             name = strings[1]
             address = strings[3]
-
-            results.append((name, address))
+            results.append((name, address))                 # 데이터의 형식을 튜플형으로(name, address)로 받는다.
 
     # 데이터의 저장
-    table = pd.DataFrame(results, columns=['name', 'address'])
+    table = pd.DataFrame(results, columns=['name', 'address'])      # pandas는 데이터를 자료구조화 시켜주는 라이브러리
     # print(table['name'])
     # print(table['address'])
     table.to_csv('{0}/pelicana_table.csv'.format(RESULT_DIRECTORY), encoding='utf-8', mode='w', index=True)
+    # csv파일로 받은 데이터를 자료구조화 시켜서 저장한다. ( 자바의 해시테이블 처럼)
 
 
 def crawling_nene():
--- Collection/Crawler.py
+++ Collection/Crawler.py
@@ -4,16 +4,16 @@ from urllib.request import Request, urlopen
 
 def crawling(url='', encoding='utf-8'):
     try:
-        request = Request(url)
-        response = urlopen(request)
+        request = Request(url)                  # url을 통해서 요청을 진행한다.
+        response = urlopen(request)             # 요청한 서버에서 응답을 받는다.
 
         try:
-            receive = response.read()
-            result = receive.decode(encoding)
+            receive = response.read()           # 받은 데이터를 receive 변수에 읽어드린다.
+            result = receive.decode(encoding)   # 읽어드린 데이터를 미리 지정해둔 코드로 인코딩한다.
         except UnicodeDecodeError:
-            result = receive.decode(encoding, 'replace')
+            result = receive.decode(encoding, 'replace')    # 만약 지정해준 인코딩에 제대로 되지않는다면 라이브러리에게 알아서 인코딩을 대체하도록 만든다.
 
         print("%s : success for request [%s]" % (datetime.now(), url))
         return result
     except Exception as e:
-        print("%s : %s" %(e, datetime.now()))
+        print("%s : %s" %(e, datetime.now()))       # 예외 처리
