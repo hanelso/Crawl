0d66b0dcebd336d4202742ccd2ecd8bb68fe7fc8
hanelso <gjsxj1992@gmail.com>
2018-09-27 17:46:52 +0900

    파이썬 웹 크롤링2 -페리카나 치킨 사이트 접속 -페리카나 매장정보 데이터화 -numpy, pandas를 이용한 자료구조화

--- Collect.py
+++ Collect.py
@@ -5,10 +5,13 @@ from urllib.request import Request, urlopen
 from bs4 import BeautifulSoup
 
 from Collection.Crawler import crawling
+import pandas as pd
 
+RESULT_DIRECTORY = "__result__"
 
 def crawling_pelicana():
-    for page in range(1, 116):
+    results = []
+    for page in count(start=1):
         html = crawling("http://www.pelicana.co.kr/store/stroe_search.html?branch_name=&gu=&si=&page=%d"%page)
 
         bs = BeautifulSoup(html, "html.parser")
@@ -16,12 +19,23 @@ def crawling_pelicana():
         tag_tbody = tag_table.find('tbody')
         tags_tr = tag_tbody.findAll('tr')
 
+        # 페이지의 끝 확인
+        if len(tags_tr) == 0:
+            break
+
         for tag_tr in tags_tr:
             strings = list(tag_tr.strings)
             name = strings[1]
             address = strings[3]
 
-            print("%s : %s" %(name, address))
+            results.append((name, address))
+
+    # 데이터의 저장
+    table = pd.DataFrame(results, columns=['name', 'address'])
+    # print(table['name'])
+    # print(table['address'])
+    table.to_csv('{0}/pelicana_table.csv'.format(RESULT_DIRECTORY), encoding='utf-8', mode='w', index=True)
+
 
 def crawling_nene():
     pass
